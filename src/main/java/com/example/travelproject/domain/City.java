3db526b5093ec52e94703de2e0f68d904b52a4fd
Алексей <fuzzylolpro@gmail.com>
2023-10-25 14:41:19 +0300

    dependency adjustment

--- src/main/java/com/example/travelproject/domain/City.java
+++ src/main/java/com/example/travelproject/domain/City.java
@@ -5,10 +5,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 @Data
@@ -19,9 +18,6 @@ public class City {
         @SequenceGenerator(name = "seq_city", sequenceName = "city_id_seq", allocationSize = 1)
         @GeneratedValue(generator = "seq_city", strategy = GenerationType.SEQUENCE)
         private Long id;
-        @Size(min = 2, max = 20)
-        @NotNull
-        @Column(name = "cities_name")
         @ManyToOne
         private Country CountryName;
 }
