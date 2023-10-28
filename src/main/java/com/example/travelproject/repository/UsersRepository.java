71d02a991cec5c6295861e13263a5a31dc8319d6
Алексей <fuzzylolpro@gmail.com>
2023-10-28 15:55:20 +0300

    @CreationTimestamp WOWOWOWOW

--- src/main/java/com/example/travelproject/repository/UsersRepository.java
+++ src/main/java/com/example/travelproject/repository/UsersRepository.java
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersRepository extends JpaRepository<Users,Long> {
-    @Override
-    <S extends Users> S save(S entity);
 }
